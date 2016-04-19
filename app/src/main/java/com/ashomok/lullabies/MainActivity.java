db51714afd67cb88a15a995ab288651e25a39ff4
Iuliia <bieliaievays@gmail.com>
2016-04-20 00:01:51 +0200

    styles for 320 added

--- app/src/main/java/com/ashomok/lullabies/MainActivity.java
+++ app/src/main/java/com/ashomok/lullabies/MainActivity.java
@@ -16,17 +16,7 @@ public class MainActivity extends Activity {
 
     private Button mBtnPlayMusic;
 //    private Button mBtnPlayMusicRandom;
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
     /**
      * Some older devices needs a small delay between UI widget updates
@@ -35,7 +25,7 @@ public class MainActivity extends Activity {
     private static final int UI_ANIMATION_DELAY = 300;
 
     private View mContentView;
-//    private View mControlsView;
+
     private boolean mVisible;
 
     @Override
@@ -45,7 +35,6 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_start);
 
         mVisible = true;
-//        mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
 
@@ -56,11 +45,6 @@ public class MainActivity extends Activity {
                 toggle();
             }
         });
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-       // findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
 
         mBtnPlayMusic = (Button) findViewById(R.id.play_music_btn);
 //        mBtnPlayMusicRandom = (Button) findViewById(R.id.play_music_random_btn);
@@ -84,21 +68,6 @@ public class MainActivity extends Activity {
         delayedHide(100);
     }
 
-//    /**
-//     * Touch listener to use for in-layout UI controls to delay hiding the
-//     * system UI. This is to prevent the jarring behavior of controls going away
-//     * while interacting with activity UI.
-//     */
-//    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-//        @Override
-//        public boolean onTouch(View view, MotionEvent motionEvent) {
-//            if (AUTO_HIDE) {
-//                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-//            }
-//            return false;
-//        }
-//    };
-
     private void toggle() {
         if (mVisible) {
             hide();
@@ -108,16 +77,10 @@ public class MainActivity extends Activity {
     }
 
     private void hide() {
-        // Hide UI first
-//        ActionBar actionBar = getSupportActionBar();
-//        if (actionBar != null) {
-//            actionBar.hide();
-//        }
-//        mControlsView.setVisibility(View.GONE);
+
         mVisible = false;
 
         // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
         mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
     }
 
@@ -148,20 +111,8 @@ public class MainActivity extends Activity {
 
         // Schedule a runnable to display UI elements after a delay
         mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
     }
 
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-//            ActionBar actionBar = getSupportActionBar();
-//            if (actionBar != null) {
-//                actionBar.show();
-//            }
-//            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
 
     private final Handler mHideHandler = new Handler();
     private final Runnable mHideRunnable = new Runnable() {
